1095e5190506fe2a0f5f835e86d738c1fa1d05a1
Alasdair Selmes <aselmes@sullivanbranding.com>
2024-06-14 12:44:26 -0500

    Add order form

--- BethanysPieShop/Models/BethanysPieShopDbContext.cs
+++ BethanysPieShop/Models/BethanysPieShopDbContext.cs
@@ -2,14 +2,16 @@
 
 namespace BethanysPieShop.Models
 {
-    public class BethanysPieShopDbContext : DbContext
-    {
-        public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext> options) : base(options)
-        {
-        }
+	public class BethanysPieShopDbContext : DbContext
+	{
+		public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext> options) : base(options)
+		{
+		}
 
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Pie> Pies { get; set; }
-        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
-    }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Pie> Pies { get; set; }
+		public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
+	}
 }
--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IPieRepository, PieRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
 builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
@@ -14,8 +14,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 {
-    options.UseSqlServer(
-        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+	options.UseSqlServer(
+		builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
 });
 
 var app = builder.Build();
@@ -24,7 +24,7 @@ app.UseStaticFiles();
 app.UseSession();
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+	app.UseDeveloperExceptionPage();
 }
 
 app.MapDefaultControllerRoute();
