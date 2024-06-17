3fa4c7083787019d0e8c5883566161eac36e343e
Alasdair Selmes <aselmes@sullivanbranding.com>
2024-06-17 12:53:57 -0500

    add Razor Pages version of checkout

--- BethanysPieShop/Models/ShoppingCart.cs
+++ BethanysPieShop/Models/ShoppingCart.cs
@@ -2,106 +2,106 @@
 
 namespace BethanysPieShop.Models
 {
-    public class ShoppingCart : IShoppingCart
-    {
-        private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+	public class ShoppingCart : IShoppingCart
+	{
+		private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
 
-        public string? ShoppingCartId { get; set; }
+		public string? ShoppingCartId { get; set; }
 
-        public List<ShoppingCartItem> ShoppingCartItems { get; set; } = default!;
+		public List<ShoppingCartItem> ShoppingCartItems { get; set; } = default!;
 
-        private ShoppingCart(BethanysPieShopDbContext bethanysPieShopDbContext)
-        {
-            _bethanysPieShopDbContext = bethanysPieShopDbContext;
-        }
+		private ShoppingCart(BethanysPieShopDbContext bethanysPieShopDbContext)
+		{
+			_bethanysPieShopDbContext = bethanysPieShopDbContext;
+		}
 
-        public static ShoppingCart GetCart(IServiceProvider services)
-        {
-            ISession? session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext?.Session;
+		public static ShoppingCart GetCart(IServiceProvider services)
+		{
+			ISession? session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext?.Session;
 
-            BethanysPieShopDbContext context = services.GetService<BethanysPieShopDbContext>() ?? throw new Exception("Error initializing");
+			BethanysPieShopDbContext context = services.GetService<BethanysPieShopDbContext>() ?? throw new Exception("Error initializing");
 
-            string cartId = session?.GetString("CartId") ?? Guid.NewGuid().ToString();
+			string cartId = session?.GetString("CartId") ?? Guid.NewGuid().ToString();
 
-            session?.SetString("CartId", cartId);
+			session?.SetString("CartId", cartId);
 
-            return new ShoppingCart(context) { ShoppingCartId = cartId };
-        }
+			return new ShoppingCart(context) { ShoppingCartId = cartId };
+		}
 
-        public void AddToCart(Pie pie)
-        {
-            var shoppingCartItem =
-                    _bethanysPieShopDbContext.ShoppingCartItems.SingleOrDefault(
-                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+		public void AddToCart(Pie pie)
+		{
+			var shoppingCartItem =
+					_bethanysPieShopDbContext.ShoppingCartItems.SingleOrDefault(
+						s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
 
-            if (shoppingCartItem == null)
-            {
-                shoppingCartItem = new ShoppingCartItem
-                {
-                    ShoppingCartId = ShoppingCartId,
-                    Pie = pie,
-                    Amount = 1
-                };
+			if (shoppingCartItem == null)
+			{
+				shoppingCartItem = new ShoppingCartItem
+				{
+					ShoppingCartId = ShoppingCartId,
+					Pie = pie,
+					Amount = 1
+				};
 
-                _bethanysPieShopDbContext.ShoppingCartItems.Add(shoppingCartItem);
-            }
-            else
-            {
-                shoppingCartItem.Amount++;
-            }
-            _bethanysPieShopDbContext.SaveChanges();
-        }
+				_bethanysPieShopDbContext.ShoppingCartItems.Add(shoppingCartItem);
+			}
+			else
+			{
+				shoppingCartItem.Amount++;
+			}
+			_bethanysPieShopDbContext.SaveChanges();
+		}
 
-        public int RemoveFromCart(Pie pie)
-        {
-            var shoppingCartItem =
-                    _bethanysPieShopDbContext.ShoppingCartItems.SingleOrDefault(
-                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+		public int RemoveFromCart(Pie pie)
+		{
+			var shoppingCartItem =
+					_bethanysPieShopDbContext.ShoppingCartItems.SingleOrDefault(
+						s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
 
-            var localAmount = 0;
+			var localAmount = 0;
 
-            if (shoppingCartItem != null)
-            {
-                if (shoppingCartItem.Amount > 1)
-                {
-                    shoppingCartItem.Amount--;
-                    localAmount = shoppingCartItem.Amount;
-                }
-                else
-                {
-                    _bethanysPieShopDbContext.ShoppingCartItems.Remove(shoppingCartItem);
-                }
-            }
+			if (shoppingCartItem != null)
+			{
+				if (shoppingCartItem.Amount > 1)
+				{
+					shoppingCartItem.Amount--;
+					localAmount = shoppingCartItem.Amount;
+				}
+				else
+				{
+					_bethanysPieShopDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+				}
+			}
 
-            _bethanysPieShopDbContext.SaveChanges();
+			_bethanysPieShopDbContext.SaveChanges();
 
-            return localAmount;
-        }
+			return localAmount;
+		}
 
-        public List<ShoppingCartItem> GetShoppingCartItems()
-        {
-            return ShoppingCartItems ??=
-                       _bethanysPieShopDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
-                           .Include(s => s.Pie)
-                           .ToList();
-        }
+		public List<ShoppingCartItem> GetShoppingCartItems()
+		{
+			return ShoppingCartItems ??=
+					   _bethanysPieShopDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+						   .Include(s => s.Pie)
+						   .ToList();
+		}
 
-        public void ClearCart()
-        {
-            var cartItems = _bethanysPieShopDbContext
-                .ShoppingCartItems
-                .Where(cart => cart.ShoppingCartId == ShoppingCartId);
+		public void ClearCart()
+		{
+			var cartItems = _bethanysPieShopDbContext
+				.ShoppingCartItems
+				.Where(cart => cart.ShoppingCartId == ShoppingCartId);
 
-            _bethanysPieShopDbContext.ShoppingCartItems.RemoveRange(cartItems);
+			_bethanysPieShopDbContext.ShoppingCartItems.RemoveRange(cartItems);
 
-            _bethanysPieShopDbContext.SaveChanges();
-        }
+			_bethanysPieShopDbContext.SaveChanges();
+		}
 
-        public decimal GetShoppingCartTotal()
-        {
-            var total = _bethanysPieShopDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
-                .Select(c => c.Pie.Price * c.Amount).Sum();
-            return total;
-        }
-    }
+		public decimal GetShoppingCartTotal()
+		{
+			var total = _bethanysPieShopDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+				.Select(c => c.Pie.Price * c.Amount).Sum();
+			return total;
+		}
+	}
 }
--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 {
@@ -28,5 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
 DbInitializer.Seed(app);
 app.Run();
